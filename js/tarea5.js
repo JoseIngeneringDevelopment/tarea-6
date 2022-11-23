57641d4527ee24919781b39738986ed10b5e2cd4
JoseRodriguez <chepealber.2@gmail.com>
2022-11-22 19:38:19 -0600

    Se agrego el archivo js

---
+++ js/tarea5.js
@@ -0,0 +1,28 @@
+
+let Guatemala = document.getElementById("ejercicio1");
+let India = document.getElementById("ejercicio2");
+let Bajos = document.getElementById("ejercicio3");
+let Suecia = document.getElementById("ejercicio4");
+let Suiza = document.getElementById("ejercicio5");
+
+function obtenerBandera() {
+    return document.getElementById('listaBanderas').value;
+}
+
+function ponerBandera() {
+    Guatemala.style.display="none";
+    India.style.display="none";
+    Bajos.style.display="none";
+    Suecia.style.display="none";
+    Suiza.style.display="none";
+
+    if (obtenerBandera()== "Guatemala") Guatemala.style.display="block";
+
+    if (obtenerBandera()== "India") India.style.display="block";
+
+    if (obtenerBandera()== "Bajos") Bajos.style.display="block";
+
+    if (obtenerBandera()== "Suecia") Suecia.style.display="block";
+
+    if (obtenerBandera()== "Suiza") Suiza.style.display="block";
+}
