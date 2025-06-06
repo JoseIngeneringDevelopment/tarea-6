09176d34c4ae512972e6f90c870fecb6b962afdf
JoseIngeneringDevelopment <77159489+JoseIngeneringDevelopment@users.noreply.github.com>
2025-06-05 19:15:36 -0600

    Add standalone French flag

--- js/tarea5.js
+++ js/tarea5.js
@@ -4,6 +4,7 @@ let India = document.getElementById("ejercicio2");
 let Bajos = document.getElementById("ejercicio3");
 let Suecia = document.getElementById("ejercicio4");
 let Suiza = document.getElementById("ejercicio5");
+let Francia = document.getElementById("ejercicio6");
 
 function obtenerBandera() {
     return document.getElementById('listaBanderas').value;
@@ -15,12 +16,16 @@ function ponerBandera() {
     Bajos.style.display="none";
     Suecia.style.display="none";
     Suiza.style.display="none";
+    if (Francia) Francia.style.display="none";
 
     if (obtenerBandera()== "Guatemala") Guatemala.style.display="block";
 
     if (obtenerBandera()== "India") India.style.display="block";
 
     if (obtenerBandera()== "Bajos") Bajos.style.display="block";
+
+    if (obtenerBandera()== "Francia" && Francia)
+        Francia.style.display="block";
 
     if (obtenerBandera()== "Suecia") Suecia.style.display="block";
 
